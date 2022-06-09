5c07f1f31b5afc3ba7292df34bb7a3b2b9810fc5
Elke Sleven <myprogrammersmail@gmail.com>
2022-06-09 22:43:51 +0200

    ..

--- examen_web_adv_2021_start/vraag2.js
+++ examen_web_adv_2021_start/vraag2.js
@@ -5,3 +5,39 @@ const countries = [
     {name: 'Russia', population: 144.4, km: 17130000},
     {name: 'China', population: 1398, km: 9597000},
 ];
+const select =  document.getElementById('countries');
+let selectTarget = '';
+const btnLargestCountry = document.querySelector('input')
+btnLargestCountry.addEventListener('click', ShowLargestCounty)
+window.onload = function (){
+    FillSelectOptions();
+    AddEventlissener();
+}
+
+function FillSelectOptions(){
+
+    for(let i = 0; i < countries.length; i++){
+        let option = document.createElement('option')
+        option.innerHTML = countries[i].name
+        select.add(option);
+    }
+}
+function AddEventlissener(){
+    select.addEventListener('change' , (e) =>{
+        const  r = document.getElementById('result')
+        selectTarget = `${e.target.value.toString()}`
+        let a = countries.find(el => el.name = selectTarget)
+        r.innerHTML = `${a.name} has a population of ${a.population} and is ${a.km} km large.`
+    })
+}
+function ShowLargestCounty(){
+    let big = countries.sort(  (a,b) =>{
+        return b.km - a.km;
+    })
+    let bigestCountry = big[0]
+    document.getElementsByTagName("p")[1].innerHTML = ` The largest coutry in this list is ${bigestCountry.name} is ` ;
+
+
+}
+
+
